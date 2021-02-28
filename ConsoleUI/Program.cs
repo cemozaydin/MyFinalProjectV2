dc58f8667fc730d6953a4ae26aa0b7b047174c0b
cemozaydin <cem.ozaydin@gmail.com>
2021-02-28 23:38:23 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,59 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+
+            //CategoryTest();
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            Console.WriteLine("----------------------GetAll()----------------------------");
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("----------------------GetByUnitPrice-----------------------");
+            foreach (var product in productManager.GetByUnitPrice(50, 100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("--------------------GetAllByCategoryId---------------------");
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("--------------------GetProductDetailDto---------------------");
+            foreach (var productDetail in productManager.GetProductDetails())
+            {
+                Console.WriteLine(productDetail.ProductId + " / " + productDetail.ProductName + " / " + productDetail.CategoryName );
+            }
+
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    // interface IEntityRepository<T> kısmına kısıt koymak istiyoruz
+    // Generic Constraint --> Generic Kısıt
+    // class : referans tip olabilir demek
+    // new () : sadece new lenebilen classları yazabilmemizi sağlar, böylece IEntitynin kendisini kullanmamış oluruz.
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); // filter = null  --> filtre vermeyebiliriz
+        T Get(Expression<Func<T, bool>> filter); // filtre zorunlu, tek data getirmek için
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
